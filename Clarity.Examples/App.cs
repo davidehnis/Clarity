589406297ec7a2db77ce671c7e54268d71cbd545
David Ehnis <david.ehnis@outlook.com>
2018-05-21 12:05:53 -0400

    added Clarity.Examples

---
+++ Clarity.Examples/App.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clarity.Examples
+{
+    public class App : IActor
+    {
+        public ISession Owner { get; set; }
+
+        public void AuthenticateUser(Credential credential)
+        {
+            // action server intention subject
+            using (var session = Session.Start(this).With<Authentication>().To<Authenticate, Credential>(credential))
+            {
+                session.WhenServerRespondsWith<UserAuthenticated>().ThenExecute(OnUserAuthenticated);
+                session.WhenServerRespondsWith<UserNotAuthenticated>().ThenExecute(OnUserNotAuthenticated);
+                session.If<ThrowsException>().ExecuteThis(HandleException);
+
+                session.Execute();
+            }
+        }
+
+        public Response Request(Request request, object data)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary></summary>
+        /// <param name="ex">ThrowsException</param>
+        private void HandleException(object ex)
+        {
+        }
+
+        private void OnUserAuthenticated()
+        {
+            Console.WriteLine("User Authenticated");
+        }
+
+        private void OnUserNotAuthenticated()
+        {
+            Console.WriteLine("User Not Authenticated");
+        }
+    }
+}
---
+++ Clarity.Examples/Credential.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clarity.Examples
+{
+    public class Credential
+    {
+        public Credential(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public string Password { get; }
+
+        public string Username { get; }
+    }
+}
